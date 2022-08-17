7cb6c9bf01ec64c09041f0bfbc1c8e2b7627e923
佐藤太一 <3101taichi@gmail.com>
2022-08-17 23:03:12 +0900

    詳細ページにcoffee_card表示

--- frontend/src/components/pages/Bean.tsx
+++ frontend/src/components/pages/Bean.tsx
@@ -1,10 +1,49 @@
+import React from "react"
 import { useParams } from "react-router-dom";
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+import { useBeanLazyQuery } from "generated/graphql";
+
+import CoffeeBeanCard from "components/organisms/BeanCard"
 
 const Bean: React.FC = () => {
+  const [bean, setBean] = React.useState<any>([])
   const { id } = useParams<{id: string}>();
+  const [getBean, {loading, data}] = useBeanLazyQuery()
+
+  React.useEffect((): any => {
+    getBean({
+      variables: {id: id}
+    })
+    setBean(data?.bean)
+  }, [data, getBean])
+
+  if (loading) {
+    return (<CircularProgress />)
+  }
+
+  if (!data) {
+    return (<CircularProgress />)
+  }
+
+  if (!bean) {
+    return (<CircularProgress />)
+  }
+
   return (
     <>
-      <h1>{id}</h1> 
+      <CoffeeBeanCard
+        id={bean.id}
+        name={bean.name}
+        processing={bean.processing || ''}
+        country={bean.country || ''}
+        varietal={bean.varietal || ''}
+        roast_level={bean.roastLevel}
+        tasting={bean.tasting || ''}
+        evaluation={bean.evaluation || 0}
+        store={bean.store.name || ''}
+        station={bean.store.station || ''}
+      />
     </>
   )
 }
